9f0b4975e550b8b01ce3da71770d0adc02f31ac9
RRomani <rromaniaguado@iesesteveterradas.cat>
2017-10-16 20:28:34 +0200

    Nuevo php para las cartas y contador de intentos

--- Tabla.php
+++ Tabla.php
@@ -1,24 +1,17 @@
 <!DOCTYPE html>
 <html>
-<meta charset="utf-8"/>
+<meta charset="utf-8" />
+
 <head>
-	<script type="text/javascript" src="Memoryjs.js"></script>
-	<link rel="stylesheet" type="text/css" href="MemoryCSS.css">
+    <script type="text/javascript" src="Memoryjs.js"></script>
+    <link rel="stylesheet" type="text/css" href="MemoryCSS.css">
 </head>
+
 <body>
     <table style="position;center">
-		<?php
-			$imagenes = [
-				0 => 'trollface.jpg',
-				1 => 'facepalm.png',
-				2 => 'yaoming.jpg',
-				3 => 'mentira.jpg',
-				4 => 'megusta.jpeg',
-				5 => 'what.jpg',
-				6 => 'cereal.png',
-				7 => 'fea.jpg',
-				8 => 'mog.jpeg'];			
-			$nivel = $_GET["nivel"];
+        <?php
+        include"cartas.php";
+        $nivel = $_GET["nivel"];
 //			$aleatorio = [];			
 //			for ($j=count($aleatorio) ; $j < ($nivel*$nivel)/2  ; $j++){
 //				$numAle = random_int(0,($nivel*$nivel)/2);
@@ -27,29 +20,29 @@
 //				print_r ($aleatorio[$j]);							
 //			}
 //			print_r($aleatorio);
-			echo "<tr><td colspan='$nivel'>MeMemory</td></tr>";
-			for($i=0 ; $i< $nivel ; $i++){
-				echo "<tr>";				
-				for($x=0 ; $x<$nivel ;$x++){
-					$numAle = random_int(0,($nivel*$nivel)/2);			
-					echo "<td>
-							<div class='flip-container' onclick='flip(event)'>
-								<div class='flipper'>
-									<div class='front'>
-										<img src='imagenes/reverso.jpeg' ></img>
-									</div>
-									<div class='back'>
-										<img src='imagenes/".$imagenes[$numAle]."'></img>
-									</div>
-								</div>
-							</div>
-						</td>";
-				}		
-				echo "</tr>";
-			}
+        echo "<tr><td colspan='$nivel'>MeMemory <br> intentos: <span id='Control'> </span></td></tr>";
+        for($i=0 ; $i< $nivel ; $i++){
+            echo "<tr>";				
+            for($x=0 ; $x<$nivel ;$x++){
+                $numAle = random_int(0,($nivel*$nivel)/2);			
+                echo "<td>
+                        <div class='flip-container' onclick='flip(event)'>
+                            <div class='flipper'>
+                                <div class='front'>
+                                    <img src='imagenes/reverso.jpeg' ></img>
+                                </div>
+                                <div class='back'>
+                                    <img src='imagenes/".$imagenes[$numAle]."'></img>
+                                </div>
+                            </div>
+                        </div>
+                    </td>";
+            }		
+            echo "</tr>";
+        }
 		?>
-		</table>
-		
-	</body>
-</html>
+    </table>
 
+</body>
+
+</html>
---
+++ cartas.php
@@ -0,0 +1,13 @@
+<?php
+    $imagenes = array(
+        0 => 'trollface.jpg',
+        1 => 'facepalm.png',
+        2 => 'yaoming.jpg',
+        3 => 'mentira.jpg',
+        4 => 'megusta.jpeg',
+        5 => 'what.jpg',
+        6 => 'cereal.png',
+        7 => 'fea.jpg',
+        8 => 'mog.jpeg'
+    );
+?>

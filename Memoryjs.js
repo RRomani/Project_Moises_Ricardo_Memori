912143f9fce16cf20f3a5252524fc558456edcb9
KrinpyOfficial <mortegarodriguez@iesesteveterradas.cat>
2017-10-17 20:31:27 +0200

    Casi esta terminado, bloqueo, victoria, mas img...

--- Memoryjs.js
+++ Memoryjs.js
@@ -1,37 +1,55 @@
+//inicializamos variables generales
 var contador = 0;
+var victoria = 0;
 var block = false;
 var carta1;
 var carta2;
 
+//funcion para cada carta con el metodo event
 function flip(event) {
-    var clase = event.currentTarget.className;
-
+    //si carta1 no tiene nada asignado, carta2 no tiene nada asignado y la variable "block" es false, a la carta1 le asignamos el div de el primer click y le cambiamos la "class" para que se voltee
     if (carta1 == null && carta2 == null && block == false) {
         carta1 = event.currentTarget;
         carta1.setAttribute("class", "flip");
+        //lo mismo que arriba pero para el click 2 ya que ahora la carta1 es not null.
     } else if (carta1 != null && carta2 == null && block == false) {
         carta2 = event.currentTarget;
         carta2.setAttribute("class", "flip");
+        //cambiamos el block a true para que no se pueda entrar en ningun if hasta cambiar la varaible block
         block = true;
     }
+    //cuando tenemos las dos cartas asignadasentramos en este if
     if (carta1 != null && carta2 != null) {
+        //con setTimeout decimos que haga la funcion en un tiempo determinado (en nuestro caso es 2000 que son 2 segundos)
         setTimeout(function () {
-            if (carta1.id == carta2.id) {
-                carta1.setAttribute("class", "blocked");
-                carta2.setAttribute("class", "blocked");
+            //si los ids coinciden, le quitamos el onclick para que se quede boca arriba y no entre a la funcion
+            if (carta1.childNodes[1].childNodes[3].getAttribute("id") == carta2.childNodes[1].childNodes[3].getAttribute("id")) {
+                carta1.setAttribute("onclick", " ");
+                carta2.setAttribute("onclick", " ");
+                //ponemos las variables a como estaban al principio
                 carta1 = null;
                 carta2 = null;
                 block = false;
+                //usamos un contador para los intentos y victoria para saber cuantas estan bloqueadas
+                victoria += 1;
+                contador += 1;
             } else {
                 carta1.setAttribute("class", "flipped");
-                event2.setAttribute("class", "flipped");
+                carta2.setAttribute("class", "flipped");
                 carta1 = null;
                 carta2 = null;
                 block = false;
+                contador += 1;
             }
-            contador += 1;
-            document.getElementById("Control").innerHTML = contador;
-        }, 2000);
 
+            document.getElementById("Control").innerHTML = contador;
+            //miramos si victoria es igual al numero de parejas del juego para saber si este a terminado
+            var nivel = document.getElementById("titulo").getAttribute("colspan");
+            if (victoria == (nivel * nivel) / 2) {
+                alert("Has ganado campeon! :3");
+                document.getElementById("restart").setAttribute("style", "display:block");
+            }
+        }, 1000);
     }
+
 }
--- Tabla.php
+++ Tabla.php
@@ -10,39 +10,55 @@
 <body>
     <table style="position;center">
         <?php
-        include"cartas.php";
-        $nivel = $_GET["nivel"];
-//			$aleatorio = [];			
-//			for ($j=count($aleatorio) ; $j < ($nivel*$nivel)/2  ; $j++){
-//				$numAle = random_int(0,($nivel*$nivel)/2);
-//				if ($numAle == $aleatorio[$j])						
-//					$aleatorio[]=$numAle;
-//				print_r ($aleatorio[$j]);							
-//			}
-//			print_r($aleatorio);
-        echo "<tr><td colspan='$nivel'>MeMemory <br> intentos: <span id='Control'> </span></td></tr>";
-        for($i=0 ; $i< $nivel ; $i++){
-            echo "<tr>";				
-            for($x=0 ; $x<$nivel ;$x++){
-                $numAle = random_int(0,($nivel*$nivel)/2);			
-                echo "<td>
-                        <div class='flip-container' onclick='flip(event)'>
-                            <div class='flipper'>
-                                <div class='front'>
-                                    <img src='imagenes/reverso.jpeg' ></img>
-                                </div>
-                                <div class='back'>
-                                    <img src='imagenes/".$imagenes[$numAle]."'></img>
-                                </div>
-                            </div>
-                        </div>
-                    </td>";
-            }		
-            echo "</tr>";
-        }
+            $cont=0;   
+			$nivel = $_GET["nivel"];
+			$imagenes = [
+				'trollface.jpg',
+				'facepalm.png',
+				'yaoming.jpg',
+				'mentira.jpg',
+				'megusta.jpeg',
+				'what.jpg',
+				'cereal.png',
+				'fea.jpg',
+                'pukerain.jpg',
+                'whynot.png',
+                'pokerface.png',
+                'lol.jpg',
+                'migusta.png',
+                'nth.jpg',
+                'chacc.jpg',
+                'no.jpg',
+                'normal.png',
+                'jackie.jpg'];
+			$aleatorio=[];		
+			for ($j=0 ; $j < ($nivel*$nivel)/2  ; $j++){					
+				$aleatorio[] = $imagenes[$j];									
+				$aleatorio[] = $imagenes[$j];				
+			}
+            shuffle($aleatorio);
+			echo "<tr><td id='titulo' colspan='$nivel'>MeMemory <br> Intentos: <span id='Control'> </span></td></tr>";
+			for($i=0 ; $i< $nivel ; $i++){
+				echo "<tr>";				
+				for($x=0 ; $x<$nivel ;$x++){
+					echo "<td>
+							<div class='flip-container' onclick='flip(event)'>
+								<div class='flipper'>
+						              <div class='front'><img src='imagenes/reverso.jpeg'></img></div>
+				                      <div class='back' id='".$aleatorio[$cont]."'><img src='imagenes/".$aleatorio[$cont]."'></img></div>
+								</div>
+							</div>
+						</td>";
+                $cont++;
+				}		
+				echo "</tr>";
+			}
 		?>
     </table>
-
+    <form action="Formulario.php" id="restart" style="display:none">
+        <button type="submit">Volver a empezar!</button>
+    </form>
+    
 </body>
 
 </html>

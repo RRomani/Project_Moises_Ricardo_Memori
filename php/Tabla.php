3a0859b25eac3d0de84da39ac8ebd63dc52840cc
KrinpyOfficial <mortegarodriguez@iesesteveterradas.cat>
2017-10-18 20:12:25 +0200

    Estilo

--- php/Tabla.php
+++ php/Tabla.php
@@ -3,19 +3,20 @@
 <meta charset="utf-8" />
 
 <head>
-    <script type="text/javascript" src="../js/Memoryjs.js"></script>
-    <link rel="stylesheet" type="text/css" href="../css/MemoryCSS.css">
+	<script type="text/javascript" src="../js/Memoryjs.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/MemoryCSS.css">
 </head>
 
-<body>
-    <table style="position;center">
-        <?php
+<body background="../imagenes/general.png" style="background-size: 100%;background-repeat: no-repeat;">
+	<div class="general">
+		<table style="position;center">
+			<?php
             //incluimos un php con el array de cartas
-            include "cartas.php";
-        
-            $cont=0;   
+			include "cartas.php";
+
+			$cont=0;   
 			$nivel = $_POST["nivel"];
-		  	
+
         //Creamos un array vacio para guardar las cartas
 			$aleatorio=[];		
         //Guardamos el numero de cartas que necesitemos en el array que acabamos de crear
@@ -24,31 +25,37 @@
 				$aleatorio[] = $imagenes[$j];				
 			}
         //Utilizamos la funcion de shuffle para mover el array de manera que desordenaremos las cartas
-            shuffle($aleatorio);
+			shuffle($aleatorio);
         //Creamos el titulo  y el tablero, para assignar la imagen tenemos un contador que nos indicara que imagen del array mostrar
-			echo "<tr><td id='titulo' colspan='$nivel'>MeMemory <br> Intentos: <span id='Control'> </span></td></tr>";
+			echo "<thead><tr><td id='titulo' colspan='$nivel'>MeMemory <br> Intentos: <span id='Control'> </span></td></tr></thead>";
 			for($i=0 ; $i< $nivel ; $i++){
 				echo "<tr>";				
 				for($x=0 ; $x<$nivel ;$x++){
 					echo "<td>
-							<div class='flip-container' onclick='flip(event)'>
-								<div class='flipper'>
-						              <div class='front'><img src='imagenes/reverso.jpeg'></img></div>
-				                      <div class='back' id='".$aleatorio[$cont]."'><img src='imagenes/".$aleatorio[$cont]."'></img></div>
-								</div>
-							</div>
-						</td>";
-                $cont++;
-				}		
-				echo "</tr>";
-			}
+					<div class='flip-container' onclick='flip(event)'>
+						<div class='flipper'>
+							<div class='front'><img src='../imagenes/reverso.jpeg'></img></div>
+							<div class='back' id='".$aleatorio[$cont]."'><img src='../imagenes/".$aleatorio[$cont]."'></img></div>
+						</div>
+					</div>
+				</td>";
+				$cont++;
+			}		
+			echo "</tr>";
+		}
 		?>
-    </table>
-    <!-- Creamos un boton para volver al principio -->
-    <form action="Formulario.php" id="restart" style="display:none">
-        <button type="submit">Volver a empezar!</button>
-    </form>
-    
+	</table>
+</div>
+<!-- Creamos un boton para volver al principio -->
+<div id="volver">
+	<form action="../Index.html" id="restart" style="display:none;">       
+		<input type="button" value="Volver a empezar!" onclick="alert('Nope, has sido troleado jajaja');"/><br><br>
+		<button type="submit">Volver a empezar!</button><br><br>
+		<input type="button" value="Volver a empezar!" onclick="alert('Nope, has sido troleado jajaja');"/><br><br>
+		<input type="button" value="Volver a empezar!" onclick="alert('Nope, has sido troleado jajaja');"/>
+	</form>
+</div>
+
 </body>
 
 </html>

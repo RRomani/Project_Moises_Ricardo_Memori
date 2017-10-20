1175a392ade43d9f2f363f7ac84bdf9ff7cf5e9c
KrinpyOfficial <mortegarodriguez@iesesteveterradas.cat>
2017-10-20 20:05:10 +0200

    Empieza la creacion del ranking

--- Puntuacion.php
+++ Puntuacion.php
@@ -3,15 +3,21 @@
 <meta charset="utf-8" />
 
 <head>
-	<script type="text/javascript" src="../js/Memoryjs.js"></script>
-	<link rel="stylesheet" type="text/css" href="../css/MemoryCSS.css">
+	<script type="text/javascript" src="js/Memoryjs.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/MemoryCSS.css">
 </head>
 
-<body background="../imagenes/general.png" style="background-size: 100%;background-repeat: no-repeat;">
+<body background="imagenes/general.png" style="background-size: 100%;background-repeat: no-repeat;">
 	<div class="general">
-		<?php
-       		echo "esto va to bien :3";
-		?>
+		<div id="formulario">
+			<h1>Formulario MEMEmory</h1>
+				<?php
+		       		$archivo = fopen("ranking/raking", "r");
+		       		while (!feof(archivo) {
+		       			fgets(handle);
+		       		}
+
+				?>
 </body>
 
 </html>

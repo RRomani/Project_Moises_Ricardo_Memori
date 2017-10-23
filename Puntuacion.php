997802982596892a9cb67598ac19087cf394fbc2
RRomani <rromaniaguado@iesesteveterradas.cat>
2017-10-23 17:35:14 +0200

    Ranking ordenado

--- Puntuacion.php
+++ Puntuacion.php
@@ -13,22 +13,38 @@
             <h1>Formulario MEMEmory</h1>
             <?php
 		       		if ($archivo = fopen("ranking/ranking", "r")){
-                        $array = array();
+                        $array[]= array();
                         while (!feof($archivo)) {
 		       			    $linea = fgets($archivo);
-                            if (!empty($array)){
+                            if (!empty($linea)){
                                 $index = strrpos($linea, "-");
                                 $nombre = substr($linea,0,$index);
                                 $puntuacion = (int) substr ($linea ,$index + 1);
-                                $array = array($nombre, $puntuacion);
+                                $array[] = array($nombre, $puntuacion);
                             }
                         }
-                    
-                    echo "Nombre: '.$array[0].'    -   Puntuación:'.$array[1].'";
+                        
+                        
+                        for($i = 1; $i < count($array); $i++){
+                            for($j = 0; $j < count($array) - $i; $j++){
+                                if ($array[$j] != null){
+                                if($array[$j][1]>$array[$j+1][1]){
+                                    $k=$array[$j+1];
+					                $array[$j+1]=$array[$j];
+					                $array[$j] = $k;
+                                    }
+                                }
+                            }
+                        }
+                        
+                        foreach($array as $registros){
+                            if (!empty($registros[0])){
+                                echo "Nombre: ".$registros[0]."    -   Puntuación: ".$registros[1]."<br>";
+                                }
+                            }
                     }
-		         	
-
 				?>
+				<input type="button" value="Volver" onclick="location='Index.html'">
 </body>
 
 </html>

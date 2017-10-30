75b8a476c9bdb73b96e4c4329052ef2db1ad3363
KrinpyOfficial <mortegarodriguez@iesesteveterradas.cat>
2017-10-30 18:00:35 +0100

    Ranking y modificar css

--- php/Ranking.php
+++ php/Ranking.php
@@ -4,12 +4,16 @@
 <?php
 	if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['nombre']) && !empty($_POST['puntuacion'])){
 		$nombre_archivo = "ranking";
- 
+		$nombre = $_POST['nombre'];
+		$puntuacion = $_POST['puntuacion'];
+		$puntuacionPersonal[] = array($nombre, $puntuacion);
+		$_SESSION['puntuacionPersonal'] = $puntuacionPersonal; 
+		unset($_SESSION['aleatorio']);
+
 		if($archivo = fopen("../ranking/".$nombre_archivo, "a")){
 			echo "Abre archivo";
 			if(fwrite($archivo, $_POST['nombre']. "-".$_POST['puntuacion']. "\n")){
-				fclose($archivo);
-                unset($_SESSION['aleatorio']);
+				fclose($archivo);                
 				header("Location: ../Index.html");
 			}else{
 				echo "No se ha podido escribir en el fichero.";
